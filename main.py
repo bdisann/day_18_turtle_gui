78e4e8721bbb73af765749a748dcd2f4005cd624
Budi Santoso <budi@jkt1.ebdesk.com>
2021-07-13 22:08:38 +0700

    import turtle module

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-class Hallo:
+from turtle import Turtle
 
+timmy_the_turtle = Turtle()
 
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-
-
-    def berjalan_maju(self):
-        return "Sedang berjalan manju"
-
-
-person_1 = Hallo('budi', 'jakarta')
-
-
-print(f"Halo, {person_1.name} dari {person_1.address}\n{person_1.name} {person_1.berjalan_maju()}")
+print(timmy_the_turtle)
