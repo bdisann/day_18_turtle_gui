52614bdca94197184b3ec747889a137c07e4817b
Budi Santoso <budi@jkt1.ebdesk.com>
2021-07-13 22:24:24 +0700

    make square by turtle

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+timmy_the_arrow = Turtle()
+timmy_the_arrow.color("blue")
 
-print(timmy_the_turtle)
+def foward_and_right():
+    timmy_the_arrow.forward(100)
+    timmy_the_arrow.right(90)
+
+def make_square():
+    num = 1
+    while num < 5:
+        foward_and_right()
+        num += 1
+
+make_square()
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
